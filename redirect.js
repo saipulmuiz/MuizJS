bc875eb6377a52e696d47a18e70ad535d7db3891
Saipul Muiz <saipulmuiz87@gmail.com>
2017-08-09 21:59:54 +0700

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+var findmove2 = navigator.userAgent;
+var redirect = findmove2.search("Opera Mini");
+if(redirect>1) {
+var queries2 = window.location.assign("googlechrome://navigate?url="+ window.location.href);
+var activity = queries2;document.getElementsByTagName('head')[0].appendChild(activity);
+}
+//]]>
+</script>
